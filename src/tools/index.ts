05eb97bc1b007ce8c7d920fbe042e4fd18cc78a3
Yiwei Ho <davidho0403@gmail.com>
2025-10-23 23:56:58 +0800

    feat: create file tool

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -4,3 +4,4 @@ export { default as grep } from "./grep";
 export { default as searchFiles } from "./search-files";
 export { default as applyEdit } from "./apply-edit";
 export { default as deleteFile } from "./delete-file";
+export { default as createFile } from "./create-file";
