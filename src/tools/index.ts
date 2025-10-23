e0b64c72c584bf3f7a5edda066e886b5a4367957
Yiwei Ho <davidho0403@gmail.com>
2025-10-23 23:29:37 +0800

    feat: apply edit tool

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -2,4 +2,5 @@ export { default as readFile } from "./read-file";
 export { default as listDir } from "./list-dir";
 export { default as grep } from "./grep";
 export { default as searchFiles } from "./search-files";
+export { default as applyEdit } from "./apply-edit";
 export { getWorkspaceRoot, resolveAndValidatePath } from "../utils/workspace";
