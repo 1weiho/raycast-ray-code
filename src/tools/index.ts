2197d8071fc505e3141b59a727647fae722b0d6e
Yiwei Ho <davidho0403@gmail.com>
2025-10-23 23:23:33 +0800

    feat: search file tool

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,4 +1,5 @@
 export { default as readFile } from "./read-file";
 export { default as listDir } from "./list-dir";
 export { default as grep } from "./grep";
+export { default as searchFiles } from "./search-files";
 export { getWorkspaceRoot, resolveAndValidatePath } from "../utils/workspace";
